23387fafc65640259377202f7142e5a5ff286f77
rupali wakde <rupaliwakde22@gmail.com>
2022-02-21 18:48:53 +0530

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Apple on 21/02/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
